ea868be52b1a34373adab0fd0754927b35df6fa7
DanieleFic <reb_ficus22@hotmail.it>
2021-12-09 14:35:34 +0100

    menu aperto

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const showmenu = document.querySelector(".fa-bars");
+
+showmenu.addEventListener('click', function(){
+
+    const element = document.querySelector ('.hamburger-menu');
+    element.className = element.classList + " active";
+});
+
+
+const closemenu = document.querySelector(".fa-times");
+
+    closemenu.addEventListener('click', function(){
+
+    
+});
