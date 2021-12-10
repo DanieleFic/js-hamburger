9a17700c0aa78ec00f06f2882d243e6ee944afed
DanieleFic <reb_ficus22@hotmail.it>
2021-12-10 10:14:10 +0100

    aggiunti commenti al js per renderlo più comprendibile

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-const showmenu = document.querySelector(".fa-bars");
+const showmenu = document.querySelector(".fa-bars"); // prendo l'elemento (icona hamburger)
 
-    showmenu.addEventListener('click', function(){
+    showmenu.addEventListener('click', function(){ // dico all icona hamburger che al click deve compiere un azione
 
     const element = document.querySelector ('.hamburger-menu');
-    element.classList.add("active")
+    element.classList.add("active") // al click aggiunge la classe active dell css che ha display block all elemento con classe hamburger menu 
 });
 
 
 
-const closemenu = document.querySelector(".fa-times");
+const closemenu = document.querySelector(".fa-times"); // prendo l'elemento (icona chiusura menu)
 
-    closemenu.addEventListener('click', function(){
+    closemenu.addEventListener('click', function(){ // dico all icona chisura menu che al click deve compiere un azione
 
     const element = document.querySelector ('.hamburger-menu');
-    element.classList.remove("active")
+    element.classList.remove("active") // al click rimuove  la classe active (che gli abbiamo dato nella funzione di prima) dell css che ha display block all elemento con classe hamburger menu 
 });
